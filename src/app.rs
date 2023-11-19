b4135b15333bdf81468a67d1425d2df20cb6695b
Carlos Fraga <cfraga@salsify.com>
2023-11-19 17:23:30 +0000

    Separate setting component from actual run

--- src/app.rs
+++ src/app.rs
@@ -1,8 +1,24 @@
-use leptos::*;
+use leptos::{*, ev::{click, MouseEvent}};
 use leptos_meta::*;
 use leptos_router::*;
 use regex::Regex;
 use rand::seq::SliceRandom;
+
+
+// pub enum Difficulty {
+//     Easy,
+//     Medium,
+//     Hard,
+// }
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct RunSettings {
+    num_words: RwSignal<usize>,
+    allowed_chars: RwSignal<String>,
+    all_words: RwSignal<bool>,
+    word_pool: RwSignal<Vec::<&'static str>>, 
+    // difficulty: RwSignal<Difficulty>,
+}
 
 #[component]
 pub fn App() -> impl IntoView {
@@ -30,15 +46,24 @@ pub fn App() -> impl IntoView {
 }
 
 
-// fn words(allowed_chars: &str) -> Vec<&str> {
-//     let existing_words = vec! [ "pata", "batata", "pena", "Pedro", "Papá", "Tia", "touro", "tempo"];
-//     let allowed_regex = Regex::new(format!("^[{}]+$", allowed_chars).as_str()).unwrap();
+fn get_word_pool(allowed_chars: Option<String>, num_words: usize) -> Vec<&'static str> {
+    let existing_words = vec! [ "pata", "batata", "pena", "Pedro", "Papá", "Tia", "touro", "tempo"];
 
-//     existing_words
-//         .iter()
-//         .filter( |word| allowed_regex.is_match(word))
-//         .collect()
-// }
+    match allowed_chars {
+        None => if existing_words.len() > num_words { existing_words.into_iter().take(num_words).collect() } else { existing_words },
+        Some(chars) => {
+            let allowed_regex = Regex::new(format!("^[{}]+$", sanitize_filter(chars)).as_str()).unwrap();
+            existing_words
+                .into_iter()
+                .filter(|word| allowed_regex.is_match(word))
+                .collect()
+        },
+    }
+}
+
+fn sanitize_filter(chars: String) -> String {
+    chars.chars().filter( |c| c.is_alphabetic()).collect()
+}
 
 fn select_word() -> &'static str {
     let existing_words = vec! [ "pata", "batata", "pena", "Pedro", "Papá", "Tia", "touro", "tempo"];
@@ -52,33 +77,74 @@ fn select_word() -> &'static str {
 #[component]
 fn HomePage() -> impl IntoView {
     // Creates a reactive value to update the button
-    let (count, set_count) = create_signal(0);
+    let settings = RunSettings {
+        num_words: create_rw_signal(10),
+        word_pool: create_rw_signal(Vec::<&str>::new()),
+        allowed_chars: create_rw_signal("ap".to_string()),
+        all_words: create_rw_signal(false),
+    };
+    
+    let is_reading = create_rw_signal(false);
     let (word, set_word) = create_signal("");
-    let (allowed_words, set_allowed_words) = create_signal("ap".to_string());
-    let (all_words, set_all_words) = create_signal(false);
+    let (count, set_count) = create_signal(0);
 
-    let on_click = move |_| {
+
+    let get_new_word = move || {
         set_count.update(|count| *count += 1);
         set_word(select_word());
+    };
+
+    let click_new_word = move |_| {
+        get_new_word();
+    };
+
+
+    let start_reading= move |i:i32| {
+        is_reading.set(true);
+        get_new_word();
     };
         
 
     view! {
+        { move || 
+            match is_reading() {
+                true => view! { 
+                    <div style="font-size: 20vw;">{word}</div>
+                    <div style="width: 100vw; height: 5vh; background-color:aquamarine;" on:click=click_new_word>"Outra Palavra!"</div>
+                    <div><span>"Já li "</span><span class="font-weight: bold;">{count}</span><span>" palavras!"</span></div>
+                }.into_view(),
+                false => view! {
+                    <SetupRun settings=settings.clone() onready=start_reading />
+                }.into_view(),
+            }
+        }
+    }
+}
+
+#[component]
+fn setup_run(settings: RunSettings, #[prop(into)] onready: Callback<i32>) -> impl IntoView {
+
+    let start_new_run = move |_| { 
+        let filter = match settings.all_words.get() {
+            true => None,
+            false=> Some(settings.allowed_chars.get()),
+        };
+
+        settings.word_pool.set(get_word_pool(filter, settings.num_words.get()));
+        onready.call(1);
+    };
+
+    view! {
         <h1>"Vamos Ler!"</h1>
-        <br/>
-        <div style="font-size: 20vw;">{word}</div>
-        <div style="width: 100vw; height: 5vh; background-color:aquamarine;" on:click=on_click>"Outra Palavra!"</div>
-        <div><span>"Já li "</span><span class="font-weight: bold;">{count}</span><span>" palavras!"</span></div>
         <div>
-            <div>
-                <span>letras permitidas: </span>  
-                <input type="text" on:input= move |e| { set_allowed_words(event_target_value(&e))} prop:value=allowed_words prop:disabled=all_words />
-            </div>
-            <div>
-                <span> usar todas as letras: </span>
-                <input type="checkbox" prop:checked=all_words on:input = move |e| { set_all_words(event_target_checked(&e))} />
-            </div>
+            <span>letras permitidas: </span>  
+            <input type="text" on:input= move |e| { settings.allowed_chars.set(event_target_value(&e))} prop:value=settings.allowed_chars prop:disabled=settings.all_words />
         </div>
+        <div>
+            <span> usar todas as letras: </span>
+            <input type="checkbox" prop:checked=settings.all_words on:input = move |e| { settings.all_words.set(event_target_checked(&e))} />
+        </div>
+        <div style="width: 100vw; height: 5vh; background-color:aquamarine;" on:click=start_new_run>"Começar!"</div>
     }
 }
 
